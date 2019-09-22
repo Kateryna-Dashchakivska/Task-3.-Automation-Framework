9b5c02b20dce36143fca683a01cb552faf98b3cd
Kateryna Dashchakivska <Kateryna_Dashchakivska@epam.com>
2019-09-22 14:23:58 +0300

    Adding project

---
+++ src/test/java/com/kateProjects/pajeObjectModel/AppTest.java
@@ -0,0 +1,20 @@
+package com.kateProjects.pajeObjectModel;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
