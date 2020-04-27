584dbe141307cd9dab8e82704e40c0b083e1c81b
Kateryna Dashchakivska <Kateryna_Dashchakivska@epam.com>
2020-04-27 23:39:53 +0300

    Fixing teardown()

--- src/main/java/com/kateProjects/po/HomePage.java
+++ src/main/java/com/kateProjects/po/HomePage.java
@@ -6,26 +6,61 @@ import org.openqa.selenium.WebElement;
 
 public class HomePage extends AbstractPage {
 
+    WebElement cart_element;
     private static final By SEARCH_INPUT_LOCATOR = By.id("search_query_top");
     private static final By GO_BUTTON_LOCATOR = By.xpath("//button[@name='submit_search']");
+    private static final By CART_BLOCK_LOCATOR = By.className("shopping_cart");
+    private static final By CART_BLOCK_REMOVE_ITEMS_LOCATOR = By.className("ajax_cart_block_remove_link");
     private static final String URL_ = "http://automationpractice.com/index.php";
 
-    public HomePage (WebDriver driver){
+    public HomePage(WebDriver driver) {
         super(driver);
     }
 
-    public void open(){
+    public void open() {
         driver.get(URL_);
     }
 
-    public void fillSearchInput (String query){
+    public void fillSearchInput(String query) {
         WebElement element = driver.findElement(SEARCH_INPUT_LOCATOR);
         element.sendKeys(query);
     }
 
-    public SearchResultsPage pressGo (){
+    public SearchResultsPage pressGo() {
         driver.findElement(GO_BUTTON_LOCATOR).click();
         return new SearchResultsPage(driver);
     }
 
+    /*public void clearCartPopup() {
+        Actions actions = new Actions(driver);
+        List<WebElement> cart_blocks = driver.findElements(CART_BLOCK_LOCATOR);
+        if (cart_blocks.isEmpty()){
+            System.out.println("No Cart element is found!");
+        } else {
+            actions.moveToElement(cart_blocks.get(0)).moveToElement(driver.findElement(By.xpath("//a[@class='ajax_cart_block_remove_link']"))).click().build().perform();
+            WebElement remove_icon = driver.findElement(CART_BLOCK_REMOVE_ITEMS_LOCATOR);
+            actions.moveToElement(remove_icon);
+            actions.click().build().perform();
+            Actions actions1 = actions.click();
+            Action actions2 = actions1.build();
+            actions2.perform();
+
+            WebElement aaa = driver.findElement(By.xpath("//a[@class='ajax_cart_block_remove_link']"));
+            WebElement aaa = driver.findElement(By.xpath("//a[@class='ajax_cart_block_remove_link']"));
+            WebElement aaa = driver.findElement(By.xpath("//div[@class='cart_block block exclusive']"));
+
+            WebDriverWait wait0 = new WebDriverWait(driver, 15);
+            wait0.until(ExpectedConditions.elementToBeClickable(aaa));
+
+            aaa.click();
+            actions.moveToElement(aaa);
+            WebElement aaa2 = ;
+
+            WebDriverWait wait = new WebDriverWait(driver, 15);
+            wait.until(ExpectedConditions.elementToBeClickable(aaa2));
+            aaa2.click();
+            actions.moveToElement(cart_blocks.get(0)).perform();
+            driver.click();
+        }
+    }*/
 }
--- src/test/java/com/kateProjects/po/TestSuit.java
+++ src/test/java/com/kateProjects/po/TestSuit.java
@@ -1,54 +1,52 @@
 package com.kateProjects.po;
 
-import org.junit.After;
-import org.junit.Before;
+import org.junit.*;
+import org.junit.rules.TestName;
+import org.junit.runners.MethodSorters;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.remote.RemoteWebDriver;
 import org.testng.Assert;
-import org.testng.annotations.AfterClass;
-import org.testng.annotations.BeforeClass;
-import org.testng.annotations.Test;
-
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.concurrent.TimeUnit;
 
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
 public class TestSuit {
 
     private static WebDriver driver;
+    private static HomePage homePage;
 
-    {
+    @Rule public TestName name = new TestName();
+
+    @Before // setup()
+    public void beforeEachTestMethod()throws MalformedURLException{
         try {
             driver = new RemoteWebDriver(
-                        new URL("http://127.0.0.1:9515"),
-                        new ChromeOptions());
+                    new URL("http://127.0.0.1:9515"),
+                    new ChromeOptions());
         } catch (MalformedURLException e) {
             e.printStackTrace();
         }
-    }
 
-    @BeforeClass(description = "Start browser")
-    private static void initBrowser() throws MalformedURLException {
-        System.setProperty("webdriver.chrome.driver", "src/main/java/resources/chromedriver.exe");
         driver.manage().timeouts().pageLoadTimeout(25, TimeUnit.SECONDS);
         driver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
         driver.manage().window().maximize();
-    }
-
-    @Before // setup()
-    public void beforeEachTestMethod() {
-        System.out.println("Setting it up!");
+        homePage = new HomePage(driver);
     }
 
     @After // tearDown()
     public void afterEachTestMethod() {
-        System.out.println("Running: tearDown");
+//        System.out.println("Running: tearDown");
+//        homePage.open();
+//        homePage.clearCartPopup();
+//        System.out.println("Cart is cleared!");
+        driver.close();
     }
 
-    @Test(description = "Price Check Test")
+    @Test
     public void PriceCheckTest() throws Exception {
-        HomePage homePage = new HomePage(driver);
+        System.out.println("Starting " + name.getMethodName());
         homePage.open();
         homePage.fillSearchInput("dress");
         SearchResultsPage searchResultsPage = homePage.pressGo();
@@ -57,9 +55,9 @@ public class TestSuit {
         Assert.assertTrue(itemPrice < 30, "Price is too expensive!");
     }
 
-    @Test(description = "Change Item Size Test")
+    @Test
     public void ChangeItemSizeTest() throws Exception {
-        HomePage homePage = new HomePage(driver);
+        System.out.println("Starting " + name.getMethodName());
         homePage.open();
         homePage.fillSearchInput("dress");
         SearchResultsPage searchResultsPage = homePage.pressGo();
@@ -70,9 +68,9 @@ public class TestSuit {
         Assert.assertTrue(size.equals("L"), "Size is not changed!");
     }
 
-    @Test(description = "Change Item Color Test")
+    @Test
     public void ChangeItemColorTest() throws Exception {
-        HomePage homePage = new HomePage(driver);
+        System.out.println("Starting " + name.getMethodName());
         homePage.open();
         homePage.fillSearchInput("dress");
         SearchResultsPage searchResultsPage = homePage.pressGo();
@@ -83,9 +81,9 @@ public class TestSuit {
         Assert.assertTrue(color.equals("Blue"), "Color is not changed!");
     }
 
-    @Test(description = "Add Item Quantity Test")
+    @Test
     public void AddItemQuantityTest() throws Exception {
-        HomePage homePage = new HomePage(driver);
+        System.out.println("Starting " + name.getMethodName());
         homePage.open();
         homePage.fillSearchInput("dress");
         SearchResultsPage searchResultsPage = homePage.pressGo();
@@ -97,25 +95,25 @@ public class TestSuit {
                 "Expected: 4 but got: " + itemQuantity);
     }
 
-    @Test(description = "Remove Item Quantity Test")
+    @Test
     public void RemoveItemQuantityTest() throws Exception {
-        HomePage homePage = new HomePage(driver);
+        System.out.println("Starting " + name.getMethodName());
         homePage.open();
         homePage.fillSearchInput("dress");
         SearchResultsPage searchResultsPage = homePage.pressGo();
         ItemPage itemPage = searchResultsPage.openFirstItem();
         itemPage.addItemsQuantity(10);
-        itemPage.removeItemQuantity(5);
+        itemPage.removeItemQuantity(3);
         CartModal cartModal = itemPage.addToCartFirstItem();
         Integer itemQuantity = cartModal.getQuantityNumber();
-        Assert.assertTrue(itemQuantity == 7, "Quantity is changed incorrectly! " +
+        Assert.assertTrue(itemQuantity == 8, "Quantity is changed incorrectly! " +
                 "Expected: 7 but got: " + itemQuantity);
         }
 
 
-    @Test(description = "Adding to Cart Test")
+    @Test
     public void AddingToCartTest() throws Exception {
-        HomePage homePage = new HomePage(driver);
+        System.out.println("Starting " + name.getMethodName());
         homePage.open();
         homePage.fillSearchInput("dress");
         SearchResultsPage searchResultsPage = homePage.pressGo();
@@ -126,8 +124,9 @@ public class TestSuit {
                 "Expected: 1 but got: " + itemQuantity);
     }
 
-     @AfterClass(description = "Close browser")
-            public static void kill(){
+
+     @AfterClass
+     public static void kill(){
         driver.quit();
      }
 
